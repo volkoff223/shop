f90b050a5eb4592251d604c554e3a331c607394a
Nicholas Voelker <volkoff223@gmail.com>
2023-12-07 10:25:49 -0500

    Added admin buttons

--- apps/web/src/routes/+layout.server.js
+++ apps/web/src/routes/+layout.server.js
@@ -1,7 +1,8 @@
 export const load = ({ locals }) => {
 	if (locals.user) {
 		return {
-			user: locals.user
+			user: locals.user,
+			isAdmin: locals.user.isAdmin
 		};
 	}
 
--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -11,45 +11,58 @@ import defaultAvatar from '$lib/assets/default-avatar.jpg'
             <a href="/" class="btn btn-ghost normal-case text-xl">ShopShop</a>
         </div>
         <div class="flex-none">
+            <!--Buttons for user not logged in-->
             {#if !data.user}
-            
             <div class="dropdown dropdown-end">
                 <a href="/login" class="btn btn-primary">Login</a>
                 <a href="/cart" class="btn btn-secondary">Cart</a>
             </div>
+            <!--Buttons for user logged in-->
             {:else}
-            <div class="dropdown dropdown-end mr-4">
-                <a href="/favorites" class="btn btn-primary btn-outline">Favorites</a>
-            </div>
-            <div class="dropdown dropdown-end mr-4">
-                <a href="/notifications" class="btn btn-primary btn-outline">Notifications</a>
-            </div>
-            <div class="dropdown dropdown-end mr-4">
-                <a href="/cart" class="btn btn-primary btn-outline">Cart</a>
-            </div>
-            <div class="dropdown dropdown-end">
-                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-                <!-- svelte-ignore a11y-label-has-associated-control -->
-                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                    <div class="w-10 rounded-full">
-                        <img src={data.user?.avatar ? getImageURL(data.user?.collectionId, data.user?.id, data.user?.avatar) : defaultAvatar} alt="user avatar" />
-                    </div>
-                </label>
-                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li>
-                        <a href="/my/orders" class="justify-between">My orders</a>
-                    </li>
-                    <li>
-                        <a href="/my/settings">Settings</a>
-                    </li>
-                    <li>
-                        <form action="/logout" method="POST">
-                            <button type="submit" class="w-full text-start">Logout</button>
-                        </form>
-                    </li>
-                </ul>
-            </div>
+                {#if data.isAdmin}
+                <!--Buttons for admin user-->
+                <p class="text-l mr-2">Admin</p>
+                <div class="dropdown dropdown-end mr-4">
+                    <a href="/products/new" class="btn btn-primary btn-outline">+New</a>
+                </div>
+                <div class="dropdown dropdown-end mr-4">
+                    <a href="/orders" class="btn btn-primary btn-outline">Orders</a>
+                </div>
+                {:else}
+                <!--Buttons for non-admin user-->
+                <div class="dropdown dropdown-end mr-4">
+                    <a href="/favorites" class="btn btn-primary btn-outline">Favorites</a>
+                </div>
+                <div class="dropdown dropdown-end mr-4">
+                    <a href="/notifications" class="btn btn-primary btn-outline">Notifications</a>
+                </div>
+                <div class="dropdown dropdown-end mr-4">
+                    <a href="/cart" class="btn btn-primary btn-outline">Cart</a>
+                </div>
+                {/if}
+                <div class="dropdown dropdown-end">
+                    <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                        <div class="w-10 rounded-full">
+                            <img src={data.user?.avatar ? getImageURL(data.user?.collectionId, data.user?.id, data.user?.avatar) : defaultAvatar} alt="user avatar" />
+                        </div>
+                    </label>
+                    <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                            <a href="/my/orders" class="justify-between">My orders</a>
+                        </li>
+                        <li>
+                            <a href="/my/settings">Settings</a>
+                        </li>
+                        <li>
+                            <form action="/logout" method="POST">
+                                <button type="submit" class="w-full text-start">Logout</button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
             {/if}
         </div>
     </nav>
